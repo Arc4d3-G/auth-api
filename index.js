ee1ce51a94d4d4f01a9f77db42fc62e1563a610a
Dewald Breed <brddew8@protonmail.com>
2025-01-01 14:21:10 +0200

    added all network binding

--- index.js
+++ index.js
@@ -212,9 +212,8 @@ async function startServer() {
   await testDbConnection();
 
   sendTestEmail();
-
-  app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
   });
 }
 
